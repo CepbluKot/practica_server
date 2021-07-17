8d4ec60ae6e2283d0381083a1eae22ca40de2a60
turba <55404866+CepbluKot@users.noreply.github.com>
2021-07-17 16:54:56 +0300

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -9,13 +9,13 @@ void loop() {
  {
   int namba;
   
-  String num = Serial.readString();
+  String stroka = Serial.readString();
   for(int x = 0 ; x < num.length(); x++){
     if (num[x] == ' ' ) {
       namba = num[x+1] - '0';
       }
     }
-  Serial.println(namba);
+  Serial.println(stroka);
   
  } 
  
